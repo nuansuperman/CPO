0e15fd58538a4c630e529452716ae2abf40a3038
cloud-dingyong <53293746+cloud-dingyong@users.noreply.github.com>
2020-05-15 20:36:15 +0800

    Add files via upload

--- lab1/src/test_immute.py
+++ lab1/src/test_immute.py
@@ -1,106 +1,132 @@
-from src.hash_immute import *
 import unittest
-from hypothesis import given
+from hypothesis import given,settings
 import hypothesis.strategies as st
+from src.hash_immute import *
 
 
-class TestImmutable(unittest.TestCase):
+class TestNode(unittest.TestCase):
+    def test_hash(self):
+        node1 = Node(None,10, None)
+        node2 = Node(None,15, None)
+        self.assertEqual(hash(node1, 5), hash(node2, 5))
+
     def test_insert(self):
-        newhash = hashmap()
-        insert(newhash, 1)
-        self.assertEqual(to_list(newhash), [1])
-        insert(newhash, 2)
-        self.assertEqual(to_list(newhash), [1, 2])
-        insert(newhash, 3)
-        self.assertEqual(to_list(newhash), [1, 2, 3])
+        buckets = [Node(0,None,None), Node(1,None,None), Node(2,None,None)]
+
+        node1 = Node(None,3, None)
+        node2 = Node(None,6, None)
+        self.assertEqual(insert(node1, buckets), insert(node2, buckets))
 
     def test_size(self):
-        newhash = hashmap()
-        insert(newhash, 1)
-        self.assertEqual(size(newhash), 1)
-        insert(newhash, 2)
-        self.assertEqual(size(newhash), 2)
-        insert(newhash, 3)
-        self.assertEqual(size(newhash), 3)
+        self.assertEqual(size(None), 0)
+        self.assertEqual(size(form(Node(1, None, None), Node(1, 'a', None))), 2)
+
+    def test_remove(self):
+        head = Node(1, None, None)
+        head.next = Node(1, 'a', None)
+        head.next.next = Node(2, 'b', None)
+        self.assertEqual(remove(form(Node(1, None, None), Node(1, 'a', None)), 'a'), Node(1, 'a', None))
 
     def test_to_list(self):
-        newhash = hashmap()
-        insert(newhash, 1)
-        insert(newhash, 2)
-        self.assertEqual(to_list(newhash), [1, 2])
+        head = Node(1, None, None)
+        node_1 = Node(1, 'a', None)
+        node_2 = Node(1, 'b', None)
+        head.next = node_1
+        node_1.next = node_2
+        self.assertEqual(to_list(head), [[1, 'a'], [1, 'b']])
 
     def test_from_list(self):
-        test = [[], [2]]
-        for l in test:
-            newhash=hashmap()
-            from_list(newhash, l)
-            self.assertEqual(size(newhash), len(l))
+        test_data = [
+            [],
+            [[None, 'a']],
+            [[None, 'a'], [None, 'b']]
+        ]
 
-    def test_reduce(self):
-        newhash = hashmap()
-        insert(newhash, 5)
-        insert(newhash, 10)
-        self.assertEqual(reduce(newhash, 'sum'), 15)
+        for e in test_data:
+            list = []
+            head = Node()
+            for i in e:
+                node = Node(i[0], i[1], None)
+                head = form(head, node)
+                list.append([i[0], i[1]])
+            list.reverse()
+            self.assertEqual(to_list(head), list)
+
+    def test_form(self):
+        self.assertEqual(form(Node(1, None, None), Node(1, 'a', None)), Node(1, None, Node(1, 'a', None)))
+
+    def test_head(self):
+        self.assertEqual(head(Node(1,'a',None)), 'a')
+
+    def test_tail(self):
+        head = Node(1, None, None)
+        node_1 = Node(1, 'a', None)
+        node_2 = Node(1, 'b', None)
+        head.next = node_1
+        node_1.next = node_2
+        self.assertEqual(tail(node_2), None)
+        self.assertEqual(tail(head), node_1)
+
 
     def test_mconcat(self):
-        newhash1 = hashmap()
-        insert(newhash1, 2)
-        insert(newhash1, 4)
-        newhash2 = hashmap()
-        insert(newhash2, 6)
-        newhash3 = hashmap()
-        insert(newhash3, 2)
-        insert(newhash3, 4)
-        insert(newhash3, 6)
-        self.assertEqual(to_list(mconcat(newhash1,newhash2)), to_list(newhash3))
+        head1 = Node(1, None, None)
+        node_1 = Node(1, 'a', None)
+        head2 = Node(2, None, None)
+        node_2 = Node(2, 'b', None)
+        head1.next = node_1
+        head2.next = node_2
+        self.assertEqual(mconcat(head1,head2), Node(1,None,Node(1,'a',Node(2,'b',None))))
 
-    def test_find_iseven(self):
-        newhash = hashmap()
-        insert(newhash, 3)
-        insert(newhash, 4)
-        self.assertEqual(find_iseven(newhash), [3])
-        insert(newhash, 5)
-        insert(newhash, 6)
-        self.assertEqual(find_iseven(newhash), [3, 5])
+    def test_remove_hashmap(self):
+        buckets = [
+            Node(None, 0, None),
+            Node(None, 1, None),
+            Node(None, 2, None),
+        ]
+        node1 = Node(None,2, None)
+        node2 = Node(None,4, None)
+        node3 = Node(None,6, None)
+        node4 = Node(None,8, None)
+        insert(node1, buckets)
+        insert(node2, buckets)
+        insert(node3, buckets)
+        insert(node4, buckets)
+        hash(node1, len(buckets))
+        hash(node2, len(buckets))
+        hash(node3, len(buckets))
+        hash(node4, len(buckets))
+        self.assertEqual(remove_hashmap(node1, buckets), 1)
 
-    def test_filter_iseven(self):
-        newhash = hashmap()
-        insert(newhash, 3)
-        insert(newhash, 4)
-        self.assertEqual(filter_iseven(newhash), [3])
-        insert(newhash, 5)
-        insert(newhash, 6)
-        self.assertEqual(filter_iseven(newhash), [3, 5])
+    @settings(max_examples=10)
+    @given(a=st.integers(), b=st.integers())
+    def test_from_list_to_list_equality(self, a, b):
+        v= [[a,b]]
+        head = from_list(v)
+
+        ans = to_list(head)
+        self.assertEqual(ans, v)
+
+    @given(a=st.integers(), b=st.integers())
+    def test_monoid_identity(self, a, b):
+        node = Node(a, b, None)
+        head1 = Node()
+        head2 = Node()
+        head2.next = node
+        self.assertEqual(mconcat(head1, head2), head2)
+        self.assertEqual(mconcat(head2, head1), head2)
 
     def test_iter(self):
-        newlist = [1, 2, 3]
-        newhash = hashmap()
-        from_list(newhash, newlist)
+        x = [[1,1], [2,2], [3,3]]
+        head = from_list(x)
         tmp = []
         try:
-            get_next = iterator(newhash)
+            get_next = iterator(head)
             while True:
                 tmp.append(get_next())
         except StopIteration:
             pass
-        self.assertEqual(newlist, tmp)
-        self.assertEqual(to_list(newhash), tmp)
-
-        get_next = iterator(None)
-        self.assertEqual(get_next(), False)
-
-    @given(st.lists(st.integers()))
-    def test_from_list_to_list_equality(self, newhash):
-        newlist = hashmap()
-        newlist = from_list(newlist, newhash)
-        a = to_list(newlist)
-        self.assertEqual(newhash, a)
-
-    @given(st.lists(st.integers()))
-    def test_from_list_to_list_equality(self, newlist):
-        newhash = hashmap()
-        newhash = from_list(newhash, newlist)
-        self.assertEqual(len(newhash), newlist)
+        self.assertEqual(x, tmp)
+        self.assertEqual(to_list(head), tmp)
 
 
 if __name__ == '__main__':
